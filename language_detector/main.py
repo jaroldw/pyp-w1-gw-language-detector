75ad3e20ba1e0c48c76fc4a65677ae64e51758c3
Jarold Wong <jaroldw@gmail.com>
2017-01-13 04:23:48 +0000

    Refactor to use list comprehension

--- language_detector/main.py
+++ language_detector/main.py
@@ -5,19 +5,10 @@
 def detect_language(text, languages):
     """Returns the detected language of given text."""
 
-    results = {}        
+    results = {}
     
     for lang in languages:
-        word_count = 0
-        
-        for word in text.split(' '):
-            if word in lang['common_words']:
-                word_count += 1
-                
-        results[lang['name']] = word_count
+        results[lang['name']] = len([word for word in text.split()
+                                    if word in lang['common_words']])
     
-    max_count = max(results.values())
-    
-    for lang, count in results.items():
-        if count == max_count:
-            return lang
+    return max(results, key=results.get)
